1edf56e22a4b02a3d6039e1cf798a11c13c48610
leo.du <leo.du@4dxtech.com>
2024-07-26 17:46:02 +0800

    remove log function redefine

--- my_logger.py
+++ my_logger.py
@@ -9,25 +9,31 @@ _INITIALIZED_ = False
 
 class MyLogger:
     def __init__(self, is_to_file, log_name, log_level):
-        # global _INITIALIZED_
-        # if not _INITIALIZED_:
-        if self.get_log_level(log_level) == logging.DEBUG:
-            formatter = logging.Formatter("%(asctime)s %(filename)s %(lineno)s %(levelname)s: %(message)s")
-        else:
-            formatter = logging.Formatter("%(asctime)s %(lineno)s %(levelname)s: %(message)s")
-        # formatter.datefmt = "%Y-%m-%d %H:%M:%S%z"
-        sh = logging.StreamHandler()
-        sh.setLevel(self.get_log_level(log_level))
-        sh.setFormatter(formatter)
-        rl = logging.getLogger()
-        rl.setLevel(self.get_log_level(logging.DEBUG))
-        rl.addHandler(sh)
-        if is_to_file:
-            fh = logging.FileHandler(filename=log_name, mode="w")
-            fh.setLevel(self.get_log_level(log_level))
-            fh.setFormatter(formatter)
-            rl.addHandler(fh)
-            # _INITIALIZED_ = True
+        global _INITIALIZED_
+        if not _INITIALIZED_:
+            if self.get_log_level(log_level) == logging.DEBUG:
+                formatter = logging.Formatter("%(asctime)s %(filename)s %(lineno)s %(levelname)s: %(message)s")
+            else:
+                formatter = logging.Formatter("%(asctime)s %(lineno)s %(levelname)s: %(message)s")
+            # formatter.datefmt = "%Y-%m-%d %H:%M:%S%z"
+            sh = logging.StreamHandler()
+            sh.setLevel(self.get_log_level(log_level))
+            sh.setFormatter(formatter)
+            self.logger = logging.getLogger()
+            self.logger.setLevel(self.get_log_level(logging.DEBUG))
+            self.logger.addHandler(sh)
+            if is_to_file:
+                fh = logging.FileHandler(filename=log_name, mode="w")
+                fh.setLevel(self.get_log_level(log_level))
+                fh.setFormatter(formatter)
+                self.logger.addHandler(fh)
+                _INITIALIZED_ = True
+
+            self.debug = self.logger.debug
+            self.info = self.logger.info
+            self.warning = self.logger.warning
+            self.error = self.logger.error
+            self.critical = self.logger.critical
 
     @staticmethod
     def get_log_level(level):
@@ -42,19 +48,3 @@ class MyLogger:
             return d[level.lower()]
         else:
             return logging.DEBUG
-
-    def debug(self, msg, *args, **kwargs):
-        logging.debug(msg, *args, **kwargs)
-
-    def info(self, msg, *args, **kwargs):
-        logging.info(msg, *args, **kwargs)
-
-    def warning(self, msg, *args, **kwargs):
-        logging.warning(msg, *args, **kwargs)
-
-    def error(self, msg, *args, **kwargs):
-        logging.error(msg, *args, **kwargs)
-
-    @staticmethod
-    def critical(self, msg, *args, **kwargs):
-        logging.critical(msg, *args, **kwargs)
